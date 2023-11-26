0fe0d478f433b40385b0096c734b6e5e99d721ac
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-11-26 23:55:59 +0700

    feat: add user create and get

---
+++ models/user/crud.go
@@ -0,0 +1,35 @@
+package user
+
+import (
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
+)
+
+// Creates a user with a starting balance of '1000.0'.
+func CreateUser(db *gorm.DB, username string) error {
+	return db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(&User{
+			Username: username,
+			Balance:  decimal.NewFromFloat32(1000.0),
+		}).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+		// return nil will commit the whole transaction
+		return nil
+	})
+}
+
+// Retrieve the user by username.
+func GetUserByUsername(db *gorm.DB, username string) (*User, error) {
+	usr := &User{}
+	err := db.Where(&User{Username: username}).First(usr).Error
+	return usr, err
+}
+
+// Retrieve the user by ID.
+func GetUser(db *gorm.DB, ID uint) (*User, error) {
+	usr := &User{}
+	err := db.First(usr, ID).Error
+	return usr, err
+}
